777eb688bbc8d39d8590016db9d386837271911a
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-06-24 17:44:50 -0300

    feat: middleware to verify cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,24 +2,42 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select('*')
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const { sessionId } = req.cookies
+
+    if (!sessionId) {
+      return res.status(401).send({
+        status: 'unauthorized',
+      })
+    }
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select('*')
 
     return {
       transactions,
     }
   })
 
-  app.get('/:id', async (req, res) => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (req, res) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getTransactionParamsSchema.parse(req.params)
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const { sessionId } = req.cookies
+
+    const transaction = await knex('transactions')
+      .where({
+        session_id: sessionId,
+        id,
+      })
+      .first()
 
     if (!transaction) {
       return res.status(404).send({ status: 'Not found' })
@@ -28,10 +46,15 @@ export async function transactionRoutes(app: FastifyInstance) {
     return { status: 'success', transaction }
   })
 
-  app.get('/summary', async () => {
+  app.get('/summary', { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies
+
     const summary = await knex('transactions')
       .sum('amount', {
         as: 'amount',
+      })
+      .where({
+        session_id: sessionId,
       })
       .first()
 
