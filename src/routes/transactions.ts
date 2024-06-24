4f7725cac83a817bf81c24d6c73f0b06307a59a7
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-06-24 16:59:19 -0300

    feat: transactions route get by id and list

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,29 @@ import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 
 export async function transactionRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (req, res) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(req.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    if (!transaction) {
+      return res.status(404).send({ status: 'Not found' })
+    }
+
+    return { status: 'success', transaction }
+  })
+
   app.post('/', async (req, res) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
