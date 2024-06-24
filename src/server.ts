32060be164615e6188e001b68ee8b9fe287e1f34
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-06-24 15:49:29 -0300

    feat: config database, setting up .env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
-app.listen({ port: 3333 }).then(() => console.log('HTTP Server running...'))
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 10000,
+    })
+    .returning('*')
+
+  return transactions
+})
+
+app.listen({ port: env.PORT }).then(() => console.log('HTTP Server running...'))
